e6e7561818dfee666810900fd27e70d3f31dcd9e
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-06-09 17:26:09 -0600

    44.TIC MIGRATOR feat: add configuration and SqlServerDataSource for database connectivity

---
+++ src/core/start/connect.ind.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm'
+import { SQLSERVER_DATABASE_NAME, SQLSERVER_PASSWORD, SQLSERVER_USERNAME } from '../../config/config.cfg'
+import { EDesti } from '../../module/tic/module/destis/db/migrator.dtb'
+
+export const SqlServerDataSource = new DataSource({
+  type: 'mssql',
+  host: 'localhost',
+  port: 1433,
+  username: SQLSERVER_USERNAME,
+  password: SQLSERVER_PASSWORD,
+  database: SQLSERVER_DATABASE_NAME,
+
+  entities: [EDesti],
+
+  synchronize: false,
+  dropSchema: false,
+
+  logging: true,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+})
