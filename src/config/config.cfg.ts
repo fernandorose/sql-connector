62cc89ca6e291fe6fe98ed5130309decb4270953
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-06-12 10:32:53 -0600

    55.TIC MIGRATOR refactor: remove unused DB_SOURCE variable and clean up configuration

--- src/config/config.cfg.ts
+++ src/config/config.cfg.ts
@@ -4,8 +4,6 @@ process.loadEnvFile()
 
 export const PORT = 3000
 
-export const DB_SOURCE = process.env.DB_SOURCE
-
 export const SQLSERVER_USERNAME = process.env.SQLSERVER_USERNAME
 export const SQLSERVER_PASSWORD = process.env.SQLSERVER_PASSWORD
 export const SQLSERVER_SERVER = process.env.SQLSERVER_SERVER
