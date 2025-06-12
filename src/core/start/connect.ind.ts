c915cf7a24d664b6d29b23ad5a2f4c703abb5406
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-06-12 09:31:21 -0600

    feat: configure SqlServerDataSource to use SQLSERVER_SERVER from config

--- src/core/start/connect.ind.ts
+++ src/core/start/connect.ind.ts
@@ -2,23 +2,20 @@ import { DataSource } from 'typeorm'
 import {
   SQLSERVER_DATABASE_NAME,
   SQLSERVER_PASSWORD,
+  SQLSERVER_SERVER,
   SQLSERVER_USERNAME,
 } from '../../config/config.cfg'
 import { EDesti } from '../../module/tic/module/destis/db/migrator.dtb'
 
 export const SqlServerDataSource = new DataSource({
   type: 'mssql',
-  host: 'localhost',
+  host: SQLSERVER_SERVER,
   port: 1433,
   username: SQLSERVER_USERNAME,
   password: SQLSERVER_PASSWORD,
   database: SQLSERVER_DATABASE_NAME,
 
   entities: [EDesti],
-
-  synchronize: false,
-  dropSchema: false,
-
   logging: true,
   options: {
     encrypt: true,
