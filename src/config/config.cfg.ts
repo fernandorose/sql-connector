3fc794b7864acaad7dfec286dee08de43ec94089
Fernando Rodriguez <ferrodriguezsegura@gmail.com>
2025-06-12 09:30:25 -0600

    50.TIC MIGRATOR refactor: remove obsolete PostgreSQL and MySQL environment variables from config

--- src/config/config.cfg.ts
+++ src/config/config.cfg.ts
@@ -6,14 +6,6 @@ export const PORT = 3000
 
 export const DB_SOURCE = process.env.DB_SOURCE
 
-export const POSTGRES_USERNAME = process.env.POSTGRES_USERNAME
-export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD
-export const POSTGRES_DATABASE_NAME = process.env.POSTGRES_DATABASE_NAME
-
-export const MYSQL_USERNAME = process.env.MYSQL_USERNAME
-export const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
-export const MYSQL_DATABASE_NAME = process.env.MYSQL_DATABASE_NAME
-
 export const SQLSERVER_USERNAME = process.env.SQLSERVER_USERNAME
 export const SQLSERVER_PASSWORD = process.env.SQLSERVER_PASSWORD
 export const SQLSERVER_SERVER = process.env.SQLSERVER_SERVER
